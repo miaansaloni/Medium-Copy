4508caf9e0e1825ad6d2978c6e10807163d0dfe6
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-02-19 00:40:57 +0100

    final fixes (2)

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,15 @@
-const navBar = document.querySelector("nav");
-const button = document.querySelector(".getStarted");
-
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 0) {
-    navBar.classList.add("scrolled");
+  const nav = document.querySelector("nav");
+  const button = document.querySelector(".getStarted");
+  const height = document.querySelector(".startReading");
+  const heightPoint = height.getBoundingClientRect().top;
+  const navHeight = nav.offsetHeight;
+
+  if (heightPoint < navHeight) {
+    nav.classList.add("scrolled");
     button.classList.add("scrolled");
   } else {
-    navBar.classList.remove("scrolled");
+    nav.classList.remove("scrolled");
     button.classList.remove("scrolled");
   }
 });
